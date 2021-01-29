0b922acf9cf8cf11b8e8831844eed85a8eb10b57
Samuel Quiñones <sdquinones1@gmail.com>
2021-01-29 10:42:01 -0500

    added more universal tags

--- src/components/StreamPi/SEO.tsx
+++ src/components/StreamPi/SEO.tsx
@@ -18,12 +18,17 @@ const SEO: React.FC<Props> = ({ title, flipOrder, description }) => {
   return (
     <Head>
       <title>{titleString}</title>
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
+      <link rel="icon" href="/favicon.ico" />
       <meta name="description" content={description} />
       <meta property="og:title" content={titleString} />
       <meta property="og:description" content={description} />
       <meta property="og:type" content="website" />
       <meta property="og:site_name" content={config.title} />
-      <meta property="og:image" content="https://stream-pi.com/images/logo.png" />
+      <meta
+        property="og:image"
+        content="https://stream-pi.com/images/logo.png"
+      />
       <meta property="og:url" content={`https://stream-pi.com/${path}`} />
       <meta property="twitter:card" content="summary" />
       <meta property="twitter:creator" content={config.social.twitter} />
