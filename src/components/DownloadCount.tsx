fbca29f634bc909054ad96eed4558d2a572b93e8
Samuel Quiñones <sdquinones1@gmail.com>
2021-01-29 11:07:12 -0500

    updated initial download count

--- src/components/DownloadCount.tsx
+++ src/components/DownloadCount.tsx
@@ -8,8 +8,8 @@ const DownloadCount: React.FC = () => {
   const [loaded, setLoaded] = useState(false);
 
   // When the repos get reset, we'll need to know initial download counts
-  const startingClient = useRef<any>(974);
-  const startingServer = useRef<any>(1288);
+  const startingClient = useRef<any>(1082);
+  const startingServer = useRef<any>(1367);
 
   useEffect(() => {
     let mounted = true;
