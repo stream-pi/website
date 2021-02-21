eab4e5c41664242457d1b01ec0e8801fe709df10
Samuel Quiñones <sdquinones1@gmail.com>
2021-02-21 13:22:58 -0500

    formatting fixes

--- .scripts/nodeVersionCheck.js
+++ .scripts/nodeVersionCheck.js
@@ -6,10 +6,10 @@ exec("node -v", function (err, stdout) {
   const ver = parseFloat(stdout.slice(1));
 
   if (ver < 12) {
-    console.log("\x1b[31m\x1b[47m%s\x1b[0m", "There was an Error:");
+    console.log("\x1b[31m\x1b[47m", "There was an Error:", "\x1b[0m");
     throw new Error("This project requires node 12 or greater");
   } else if (ver === 13) {
-    console.log("\x1b[31m\x1b[47m%s\x1b[0m", "There was an Error:");
+    console.log("\x1b[31m\x1b[47m", "There was an Error:", "\x1b[0m");
     throw new Error(
       "As of NextJS 10.0.6, Node 13 no longer works, please upgrade to LTS"
     );
@@ -17,9 +17,10 @@ exec("node -v", function (err, stdout) {
 
   if (ver < 14) {
     console.log(
-      "\x1b[33m%s\x1b[0m",
-      "It is recommended that you use the most recent LTS build of node: https://nodejs/org/en/download/"
+      "\x1b[33m",
+      "It is recommended that you use the most recent LTS build of node: https://nodejs/org/en/download/",
+      "\x1b[0m"
     );
-    console.log("\x1b[33m%s\x1b[0m", `You are using ${stdout}`);
+    console.log("\x1b[33m", `You are using ${stdout}`, "\x1b[0m");
   }
 });
--- .scripts/npmVersionCheck.js
+++ .scripts/npmVersionCheck.js
@@ -3,18 +3,19 @@ const exec = require("child_process").exec;
 exec("npm -v", function (err, stdout) {
   if (err) throw err;
 
-  const ver = parseFloat(stdout.slice(0));
+  const ver = parseInt(stdout.slice(0));
 
   if (ver < 6) {
-    console.log("\x1b[31m\x1b[47m%s\x1b[0m", "There was an Error:");
+    console.log("\x1b[31m\x1b[47m", "There was an Error:", "\x1b[0m");
     throw new Error("This project requires npm 6 or greater");
   }
 
   if (ver > 6) {
     console.log(
-      "\x1b[33m%s\x1b[0m",
-      "This project has not been tested on NPM versions newer than 6, please report any problems to:\nhttps://github.com/stream-pi/website/issues"
+      "\x1b[33m",
+      "This project has not been tested on NPM versions newer than 6, please report any problems to:\nhttps://github.com/stream-pi/website/issues",
+      "\x1b[0m"
     );
-    console.log("\x1b[33m%s\x1b[0m", `You are using ${stdout}`);
+    console.log("\x1b[33m", `You are using ${stdout}`, "\x1b[0m");
   }
 });
