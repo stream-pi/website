d0b9d3ff70c54d8b61b1cc076a637e0bcc41e273
Samuel Quiñones <sdquinones1@gmail.com>
2021-02-27 15:07:19 -0500

    simplified

--- src/helpers/InstallHelper.ts
+++ src/helpers/InstallHelper.ts
@@ -1,36 +1,10 @@
 export const Software = [
   {
     name: "Server",
-    platforms: [
-      {
-        platform: "Windows",
-        install: ["Download", "run"],
-      },
-      {
-        platform: "Linux",
-        install: ["Download", "run"],
-      },
-    ],
+    platforms: ["Windows", "Linux"],
   },
   {
     name: "Client",
-    platforms: [
-      {
-        platform: "Android",
-        install: ["Download", "run"],
-      },
-      {
-        platform: "Linux",
-        install: ["Download", "run"],
-      },
-      {
-        platform: "RaspberryPi",
-        install: ["Download", "Run"],
-      },
-      {
-        platform: "Windows",
-        install: ["Download", "run"],
-      },
-    ],
+    platforms: ["Android", "Linux", "Raspberry-Pi", "Windows"],
   },
 ];
