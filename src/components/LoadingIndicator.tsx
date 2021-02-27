4d587a270601825872506c0a544129362f942bcf
Samuel Quiñones <sdquinones1@gmail.com>
2021-02-27 16:50:26 -0500

    removed unused HOC

--- src/components/LoadingIndicator.tsx
+++ src/components/LoadingIndicator.tsx
@@ -1,9 +1,5 @@
 import React from "react";
 
-type Props = {
-  loaded: boolean;
-};
-
 export const LoadingIndicator: React.FC = () => {
   return (
     <div className="lds-ellipsis">
@@ -14,8 +10,3 @@ export const LoadingIndicator: React.FC = () => {
     </div>
   );
 };
-
-export const IsLoadingHOC = <P extends object>(
-  Component: React.ComponentType<P>
-): React.FC<P & Props> => ({ loaded, ...props }: Props) =>
-  !loaded ? <LoadingIndicator /> : <Component {...(props as P)} />;
