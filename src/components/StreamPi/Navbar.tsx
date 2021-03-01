9a08a96c3be249779bb98c3f0e627b14d7131323
Samuel Quiñones <sdquinones1@gmail.com>
2021-03-01 18:39:39 -0500

    added navbar brand at small breakpoints

--- src/components/StreamPi/Navbar.tsx
+++ src/components/StreamPi/Navbar.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
+import Image from "react-bootstrap/Image";
 
 const StreamPiNavbar: React.FC<{ navVariant: "light" | "dark" }> = ({
   navVariant,
@@ -8,6 +9,9 @@ const StreamPiNavbar: React.FC<{ navVariant: "light" | "dark" }> = ({
 }) => {
   return (
     <Navbar collapseOnSelect expand="sm" variant={navVariant} fixed="top">
+      <Navbar.Brand className="d-sm-none">
+        <Image src="/favicon.ico" height="32px" />
+      </Navbar.Brand>
       <Navbar.Toggle aria-controls="streampi-navbar" />
       <Navbar.Collapse id="streampi-navbar">
         <Nav className="mx-auto">{children}</Nav>
