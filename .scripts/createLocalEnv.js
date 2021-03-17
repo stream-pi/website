d6e6c23e38b3b0d6e98f66a3b0753027196c7027
Samuel Quiñones <sdquinones1@gmail.com>
2021-03-16 22:34:46 -0400

    improved script
    - added better error catching
    - added nullish defaults

--- .scripts/createLocalEnv.js
+++ .scripts/createLocalEnv.js
@@ -9,7 +9,7 @@ const environment = process.argv.slice(2)[0];
 
 const prod_env = {
   host: "",
-  port: "n",
+  port: "",
 };
 
 switch (environment) {
@@ -18,23 +18,24 @@ switch (environment) {
       "What is the HOST / URL? (include http(s)://)\n",
       function (host) {
         rl.question("What is the port? (type 'n' for none)\n", function (port) {
-          prod_env.host = host;
-          prod_env.port = port === "n" ? "" : `:${port}`;
+          prod_env.host = host.toLowerCase() || "http://localhost";
+          prod_env.port = port.toLowerCase() === "n" ? "" : `:${port || "8080"}`;
           rl.close();
         });
       }
     );
 
-    // rl.on("SIGINT", function () {
-    //   console.log("INTERRUPT");
-    //   process.exit(0);
-    // });
+    // prevents bad data from being injected
+    rl.on("SIGINT", function () {
+      console.log("INTERRUPT");
+      process.exit(0);
+    });
 
     rl.on("close", function () {
       // if (!fs.existsSync(".env.production.local")) {
       fs.writeFileSync(
         ".env.production.local",
-        'NEXT_PUBLIC_BASE_URL="http://localhost:8080"'
+        `NEXT_PUBLIC_BASE_URL="${prod_env.host}${prod_env.port}"`
       );
       // } else {
       //   console.log("File already exists");
