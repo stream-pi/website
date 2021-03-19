22b4b30e6ad40d15f13b30111e83e9cbdeffc5b0
Samuel Quiñones <sdquinones1@gmail.com>
2021-03-19 17:32:27 -0400

    switched to the ref forwarding version

--- src/components/ThemedButton.tsx
+++ src/components/ThemedButton.tsx
@@ -1,28 +1,28 @@
 import React from "react";
-import Button from "react-bootstrap/Button";
+import Button, { ButtonProps } from "react-bootstrap/Button";
 
-type IProps = React.ComponentProps<Button>;
-export default class ThemedButton extends React.Component<IProps> {
-  render() {
-    const { variant, ...rest } = this.props;
+// type IProps = React.ComponentProps<Button>;
+// export default class ThemedButton extends React.Component<IProps> {
+//   render() {
+//     const { variant, ...rest } = this.props;
 
-    return global.isLightMode ? (
-      <Button variant={`outline-${variant || "primary"}`} {...rest} />
-    ) : (
-      <Button variant={variant || "primary"} {...rest} />
-    );
-  }
-}
+//     return global.isLightMode ? (
+//       <Button variant={`outline-${variant || "primary"}`} {...rest} />
+//     ) : (
+//       <Button variant={variant || "primary"} {...rest} />
+//     );
+//   }
+// }
 
-// export const ThemedButton = React.forwardRef<any, IProps>((props, ref) => {
-//   const { variant, ...rest } = props;
-//   return global.isLightMode ? (
-//     <Button ref={ref} variant={`outline-${variant || "primary"}`} {...rest} />
-//   ) : (
-//     <Button ref={ref} variant={variant || "primary"} {...rest} />
-//   );
-// });
+export const ThemedButton = React.forwardRef<any, ButtonProps>((props, ref) => {
+  const { variant, ...rest } = props;
+  return global.isLightMode ? (
+    <Button ref={ref} variant={`outline-${variant || "primary"}`} {...rest} />
+  ) : (
+    <Button ref={ref} variant={variant || "primary"} {...rest} />
+  );
+});
 
-// ThemedButton.displayName = "ThemedButton";
+ThemedButton.displayName = "ThemedButton"; // required because of eslint
 
-// export default ThemedButton;
+export default ThemedButton;
