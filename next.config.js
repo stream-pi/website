221c274daa36600a3c7b5fd155936c502fc659fe
Samuel Quiñones <sdquinones1@gmail.com>
2021-06-17 22:22:22 -0400

    removed `first` page

--- next.config.js
+++ next.config.js
@@ -3,6 +3,11 @@ module.exports = {
   async redirects() {
     return [
       {
+        source: "/first",
+        destination: "/",
+        permanent: true,
+      },
+      {
         source: "/download/:sercli*",
         destination: "/install/:sercli*", // Matched parameters can be used in the destination
         permanent: true,
--- src/components/Page/First/index.ts
+++
@@ -1,4 +0,0 @@
-export { default as FirstCardComp } from "./_FirstCardComp";
-export { default as FirstFeatRow } from "./_FirstFeatRow";
-export { ChunkedFirst, FirstItems } from "./Helper";
-export type { FirstCard } from "./Helper";
