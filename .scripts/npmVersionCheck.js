5c2bef902b004ef9a797c0914cb4558789afe750
Samuel Quiñones <sdquinones1@gmail.com>
2022-01-08 16:06:30 -0500

    script change

--- .scripts/npmVersionCheck.js
+++ .scripts/npmVersionCheck.js
@@ -5,16 +5,8 @@ exec("npm -v", function (err, stdout) {
 
   const ver = parseInt(stdout.slice(0));
 
-  if (ver < 6) {
+  if (ver < 7) {
     console.log("\x1b[31m\x1b[47m", "There was an Error:", "\x1b[0m");
-    throw new Error("This project requires npm 6 or greater");
-  }
-
-  if (ver > 6) {
-    console.log(
-      "\x1b[33m",
-      "We are slowly trying to upgrade to 7, please use LTS / 6 for now",
-      "\x1b[0m"
-    );
+    throw new Error("This project requires npm 7 or greater");
   }
 });
