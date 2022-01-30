f62aec74e95498cc2d901b1180ad39e20f7cc70d
Samuel Quiñones <sdquinones1@gmail.com>
2022-01-30 00:24:16 -0500

    updated to latest

--- src/modules/Install/Helper/client.ts
+++ src/modules/Install/Helper/client.ts
@@ -1,43 +1,55 @@
 export default {
-  Version: "1.0.0-EA+4-SNAPSHOT",
+  Version: "2.0.0-SNAPSHOT",
   ReleasePage:
-    "https://github.com/stream-pi/client/releases/tag/1.0.0-EA%2B4-SNAPSHOT",
+    "https://github.com/stream-pi/client/releases/tag/2.0.0-SNAPSHOT",
   Downloads: [
     {
-      Name: "stream-pi-client-android-aarch64-1.0.0-EA+4-SNAPSHOT.apk",
-      Link: "https://github.com/stream-pi/client/releases/download/1.0.0-EA%2B4-SNAPSHOT/stream-pi-client-android-aarch64-1.0.0-EA%2B4-SNAPSHOT.apk",
+      Name: "stream-pi-client-android-aarch64-2.0.0-SNAPSHOT.apk",
+      Link: "https://github.com/stream-pi/client/releases/download/2.0.0-SNAPSHOT/stream-pi-client-android-aarch64-2.0.0-SNAPSHOT.apk",
     },
     {
-      Name: "stream-pi-client-linux-aarch64-1.0.0-EA+4-SNAPSHOT-jvm-executable.zip",
-      Link: "https://github.com/stream-pi/client/releases/download/1.0.0-EA%2B4-SNAPSHOT/stream-pi-client-linux-aarch64-1.0.0-EA%2B4-SNAPSHOT-jvm-executable.zip",
+      Name: "stream-pi-client-linux-aarch64-2.0.0-SNAPSHOT-jvm-executable.zip",
+      Link: "https://github.com/stream-pi/client/releases/download/2.0.0-SNAPSHOT/stream-pi-client-linux-aarch64-2.0.0-SNAPSHOT-jvm-executable.zip",
     },
     {
-      Name: "stream-pi-client-linux-aarch64-1.0.0-EA+4-SNAPSHOT-native-executable.zip",
-      Link: "https://github.com/stream-pi/client/releases/download/1.0.0-EA%2B4-SNAPSHOT/stream-pi-client-linux-aarch64-1.0.0-EA%2B4-SNAPSHOT-native-executable.zip",
+      Name: "stream-pi-client-linux-aarch64-2.0.0-SNAPSHOT-native-executable.zip",
+      Link: "https://github.com/stream-pi/client/releases/download/2.0.0-SNAPSHOT/stream-pi-client-linux-aarch64-2.0.0-SNAPSHOT-native-executable.zip",
     },
     {
-      Name: "stream-pi-client-linux-arm32-1.0.0-EA+4-SNAPSHOT-executable.zip",
-      Link: "https://github.com/stream-pi/client/releases/download/1.0.0-EA%2B4-SNAPSHOT/stream-pi-client-linux-arm32-1.0.0-EA%2B4-SNAPSHOT-executable.zip",
+      Name: "stream-pi-client-linux-arm32-2.0.0-SNAPSHOT-executable.zip",
+      Link: "https://github.com/stream-pi/client/releases/download/2.0.0-SNAPSHOT/stream-pi-client-linux-arm32-2.0.0-SNAPSHOT-executable.zip",
     },
     {
-      Name: "stream-pi-client-linux-x64-1.0.0-EA+4-SNAPSHOT-executable.zip",
-      Link: "https://github.com/stream-pi/client/releases/download/1.0.0-EA%2B4-SNAPSHOT/stream-pi-client-linux-x64-1.0.0-EA%2B4-SNAPSHOT-executable.zip",
+      Name: "stream-pi-client-linux-x64-2.0.0-SNAPSHOT.AppImage",
+      Link: "https://github.com/stream-pi/client/releases/download/2.0.0-SNAPSHOT/stream-pi-client-linux-x64-2.0.0-SNAPSHOT.AppImage",
     },
     {
-      Name: "stream-pi-client-linux-x64-1.0.0-EA+4-SNAPSHOT.deb",
-      Link: "https://github.com/stream-pi/client/releases/download/1.0.0-EA%2B4-SNAPSHOT/stream-pi-client-linux-x64-1.0.0-EA%2B4-SNAPSHOT.deb",
+      Name: "stream-pi-client-linux-x64-2.0.0-SNAPSHOT.deb",
+      Link: "https://github.com/stream-pi/client/releases/download/2.0.0-SNAPSHOT/stream-pi-client-linux-x64-2.0.0-SNAPSHOT.deb",
     },
     {
-      Name: "stream-pi-client-linux-x64-1.0.0-EA+4-SNAPSHOT.rpm",
-      Link: "https://github.com/stream-pi/client/releases/download/1.0.0-EA%2B4-SNAPSHOT/stream-pi-client-linux-x64-1.0.0-EA%2B4-SNAPSHOT.rpm",
+      Name: "stream-pi-client-linux-x64-2.0.0-SNAPSHOT.rpm",
+      Link: "https://github.com/stream-pi/client/releases/download/2.0.0-SNAPSHOT/stream-pi-client-linux-x64-2.0.0-SNAPSHOT.rpm",
     },
     {
-      Name: "stream-pi-client-macos-x64-1.0.0-EA+4-SNAPSHOT.pkg",
-      Link: "https://github.com/stream-pi/client/releases/download/1.0.0-EA%2B4-SNAPSHOT/stream-pi-client-macos-x64-1.0.0-EA%2B4-SNAPSHOT.pkg",
+      Name: "stream-pi-client-linux-x86-2.0.0-SNAPSHOT.AppImage",
+      Link: "https://github.com/stream-pi/client/releases/download/2.0.0-SNAPSHOT/stream-pi-client-linux-x86-2.0.0-SNAPSHOT.AppImage",
     },
     {
-      Name: "stream-pi-client-windows-x64-1.0.0-EA+4-SNAPSHOT-installer.msi",
-      Link: "https://github.com/stream-pi/client/releases/download/1.0.0-EA%2B4-SNAPSHOT/stream-pi-client-windows-x64-1.0.0-EA%2B4-SNAPSHOT-installer.msi",
+      Name: "stream-pi-client-linux-x86-2.0.0-SNAPSHOT.deb",
+      Link: "https://github.com/stream-pi/client/releases/download/2.0.0-SNAPSHOT/stream-pi-client-linux-x86-2.0.0-SNAPSHOT.deb",
+    },
+    {
+      Name: "stream-pi-client-linux-x86-2.0.0-SNAPSHOT.rpm",
+      Link: "https://github.com/stream-pi/client/releases/download/2.0.0-SNAPSHOT/stream-pi-client-linux-x86-2.0.0-SNAPSHOT.rpm",
+    },
+    {
+      Name: "stream-pi-client-macos-x64-2.0.0-SNAPSHOT.pkg",
+      Link: "https://github.com/stream-pi/client/releases/download/2.0.0-SNAPSHOT/stream-pi-client-macos-x64-2.0.0-SNAPSHOT.pkg",
+    },
+    {
+      Name: "stream-pi-client-windows-x64-2.0.0-SNAPSHOT-installer.msi",
+      Link: "https://github.com/stream-pi/client/releases/download/2.0.0-SNAPSHOT/stream-pi-client-windows-x64-2.0.0-SNAPSHOT-installer.msi",
     },
   ],
 };
--- src/modules/Install/Helper/server.ts
+++ src/modules/Install/Helper/server.ts
@@ -1,35 +1,51 @@
 export default {
-  Version: "1.0.0-EA+4-SNAPSHOT",
+  Version: "2.0.0-SNAPSHOT",
   ReleasePage:
-    "https://github.com/stream-pi/server/releases/tag/1.0.0-EA%2B4-SNAPSHOT",
+    "https://github.com/stream-pi/server/releases/tag/2.0.0-SNAPSHOT",
   Downloads: [
     {
-      Name: "stream-pi-server-linux-aarch64-1.0.0-EA+4-SNAPSHOT-executable.zip",
-      Link: "https://github.com/stream-pi/server/releases/download/1.0.0-EA%2B4-SNAPSHOT/stream-pi-server-linux-aarch64-1.0.0-EA%2B4-SNAPSHOT-executable.zip",
+      Name: "stream-pi-server-linux-aarch64-2.0.0-SNAPSHOT-executable.zip",
+      Link: "https://github.com/stream-pi/server/releases/download/2.0.0-SNAPSHOT/stream-pi-server-linux-aarch64-2.0.0-SNAPSHOT-executable.zip",
     },
     {
-      Name: "stream-pi-server-linux-arm32-1.0.0-EA+4-SNAPSHOT-executable.zip",
-      Link: "https://github.com/stream-pi/server/releases/download/1.0.0-EA%2B4-SNAPSHOT/stream-pi-server-linux-arm32-1.0.0-EA%2B4-SNAPSHOT-executable.zip",
+      Name: "stream-pi-server-linux-arm32-2.0.0-SNAPSHOT-executable.zip",
+      Link: "https://github.com/stream-pi/server/releases/download/2.0.0-SNAPSHOT/stream-pi-server-linux-arm32-2.0.0-SNAPSHOT-executable.zip",
     },
     {
-      Name: "stream-pi-server-linux-x64-1.0.0-EA+4-SNAPSHOT-executable.zip",
-      Link: "https://github.com/stream-pi/server/releases/download/1.0.0-EA%2B4-SNAPSHOT/stream-pi-server-linux-x64-1.0.0-EA%2B4-SNAPSHOT-executable.zip",
+      Name: "stream-pi-server-linux-x64-2.0.0-SNAPSHOT.AppImage",
+      Link: "https://github.com/stream-pi/server/releases/download/2.0.0-SNAPSHOT/stream-pi-server-linux-x64-2.0.0-SNAPSHOT.AppImage",
     },
     {
-      Name: "stream-pi-server-linux-x64-1.0.0-EA+4-SNAPSHOT.deb",
-      Link: "https://github.com/stream-pi/server/releases/download/1.0.0-EA%2B4-SNAPSHOT/stream-pi-server-linux-x64-1.0.0-EA%2B4-SNAPSHOT.deb",
+      Name: "stream-pi-server-linux-x64-2.0.0-SNAPSHOT.deb",
+      Link: "https://github.com/stream-pi/server/releases/download/2.0.0-SNAPSHOT/stream-pi-server-linux-x64-2.0.0-SNAPSHOT.deb",
     },
     {
-      Name: "stream-pi-server-linux-x64-1.0.0-EA+4-SNAPSHOT.rpm",
-      Link: "https://github.com/stream-pi/server/releases/download/1.0.0-EA%2B4-SNAPSHOT/stream-pi-server-linux-x64-1.0.0-EA%2B4-SNAPSHOT.rpm",
+      Name: "stream-pi-server-linux-x64-2.0.0-SNAPSHOT.rpm",
+      Link: "https://github.com/stream-pi/server/releases/download/2.0.0-SNAPSHOT/stream-pi-server-linux-x64-2.0.0-SNAPSHOT.rpm",
     },
     {
-      Name: "stream-pi-server-macos-x64-1.0.0-EA+4-SNAPSHOT.pkg",
-      Link: "https://github.com/stream-pi/server/releases/download/1.0.0-EA%2B4-SNAPSHOT/stream-pi-server-macos-x64-1.0.0-EA%2B4-SNAPSHOT.pkg",
+      Name: "stream-pi-server-linux-x86-2.0.0-SNAPSHOT.AppImage",
+      Link: "https://github.com/stream-pi/server/releases/download/2.0.0-SNAPSHOT/stream-pi-server-linux-x86-2.0.0-SNAPSHOT.AppImage",
     },
     {
-      Name: "stream-pi-server-windows-x64-1.0.0-EA+4-SNAPSHOT-installer.msi",
-      Link: "https://github.com/stream-pi/server/releases/download/1.0.0-EA%2B4-SNAPSHOT/stream-pi-server-windows-x64-1.0.0-EA%2B4-SNAPSHOT-installer.msi",
+      Name: "stream-pi-server-linux-x86-2.0.0-SNAPSHOT.deb",
+      Link: "https://github.com/stream-pi/server/releases/download/2.0.0-SNAPSHOT/stream-pi-server-linux-x86-2.0.0-SNAPSHOT.deb",
+    },
+    {
+      Name: "stream-pi-server-linux-x86-2.0.0-SNAPSHOT.rpm",
+      Link: "https://github.com/stream-pi/server/releases/download/2.0.0-SNAPSHOT/stream-pi-server-linux-x86-2.0.0-SNAPSHOT.rpm",
+    },
+    {
+      Name: "stream-pi-server-macos-x64-2.0.0-SNAPSHOT.pkg",
+      Link: "https://github.com/stream-pi/server/releases/download/2.0.0-SNAPSHOT/stream-pi-server-macos-x64-2.0.0-SNAPSHOT.pkg",
+    },
+    {
+      Name: "stream-pi-server-windows-x64-2.0.0-SNAPSHOT-installer.msi",
+      Link: "https://github.com/stream-pi/server/releases/download/2.0.0-SNAPSHOT/stream-pi-server-windows-x64-2.0.0-SNAPSHOT-installer.msi",
+    },
+    {
+      Name: "stream-pi-server-windows-x86-2.0.0-SNAPSHOT-installer.msi",
+      Link: "https://github.com/stream-pi/server/releases/download/2.0.0-SNAPSHOT/stream-pi-server-windows-x86-2.0.0-SNAPSHOT-installer.msi",
     },
   ],
 };
